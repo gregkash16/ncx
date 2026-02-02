e37ff5ca92a9cb9d59f110e240e2df4ee6690884
gregkash16 <gregkash@gmail.com>
2026-02-02 08:54:42 -0500

    Update route.ts

--- src/app/api/players/route.ts
+++ src/app/api/players/route.ts
@@ -41,7 +41,7 @@ export async function GET(req: Request) {
         games,
         win_pct,
         ppg,
-        s1, s2, s3, s4, s5, s6, s7, s8, s9
+        s1, s2, s3, s4, s5, s6, s7, s8, s9,
         championships
       FROM S9.all_time_stats
       ORDER BY id ASC
