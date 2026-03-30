8725fd77343d12ede96963212ea06f137968e233
gregkash16 <gregkash@gmail.com>
2026-03-29 20:51:51 -0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,17 +1,13 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
-const isDev = process.env.NODE_ENV === 'development';
-
 const config: CapacitorConfig = {
   appId: 'com.ncx.app',
   appName: 'NCX',
   webDir: 'public',
-  server: isDev
-    ? {
-        url: 'https://nickelcityxwing.com',
-        cleartext: false,
-      }
-    : undefined,
+  server: {
+    url: 'https://nickelcityxwing.com',
+    cleartext: false,
+  },
   plugins: {
     SplashScreen: {
       launchShowDuration: 0,
